8edb20313cce8a7290ddeb50e45c28a99938a2d9
mohitsolia <mohit.solia@gmail.com>
2025-01-14 11:27:15 +0530

    add current data

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 console.log('hello javascript')
 console.log('update hello world')
-<<<<<<< HEAD
 console.log('welcome javascript')
-=======
-console.log('hello')
->>>>>>> 0197d614e2d36129e224c5ded3fac95da1042571
